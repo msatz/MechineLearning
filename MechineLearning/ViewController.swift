5167e4e10c6906062280413d883fcb6f05ccd8a2
smac <smac@smacs-mac-mini.local>
2018-10-23 12:23:07 +0530

    Initial Commit

---
+++ MechineLearning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MechineLearning
+//
+//  Created by smac on 10/23/18.
+//  Copyright © 2018 msatz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
