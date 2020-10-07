51e3d30b714a3948ff5225cd00017bbe1866e02c
Sathish mohan <sathishmsdr@gmail.com>
2020-10-07 17:51:56 +0530

    Detect objects

--- MechineLearning/ViewController.swift
+++ MechineLearning/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
+import CoreML
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView:UIImageView!
+    @IBOutlet weak var ImageLabel: UILabel!
+    //     var model: Inceptionv3!
+    var model: Flowers!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        model = Flowers()
     }
-
-
+    
+    @IBAction func chooseSource(_ sender: Any){
+        let imagePickerView = UIImagePickerController()
+        imagePickerView.delegate = self
+        let actionSheet = UIAlertController(title: "Choose Photo Source", message: nil, preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler:{(action:UIAlertAction) in
+            imagePickerView.sourceType = .camera
+            self.present(imagePickerView, animated: true, completion: nil)
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Photos", style: .default, handler : { (action:UIAlertAction) in
+            imagePickerView.sourceType = .photoLibrary
+            self.present(imagePickerView, animated: true, completion: nil)
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true)
+        ImageLabel.text = "Analyzing Image..."
+        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
+            return
+        }
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 227, height: 227), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 227, height: 227))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return
+        }
+        // MARK: -
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        context?.translateBy(x: 0, y: newImage.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        //let imageCentered = cropToCenter(image: image)
+        UIGraphicsPushContext(context!)
+        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        imageView.image = newImage
+        guard let prediction = try? model.prediction(data: pixelBuffer!)else {
+            return
+        }
+        ImageLabel.text = "You are looking at \(prediction.classLabel)."
+    }
 }
-
